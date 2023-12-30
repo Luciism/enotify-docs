43e8b61542f4cd517705f82b2eea736cb0d539cb
Depleted <odepleted@gmail.com>
2023-12-31 01:10:57 +1300

    changed docs hostname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.enotify.lucism.dev/',
+  url: 'https://enotify-docs.lucism.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
